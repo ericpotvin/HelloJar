5e9c5d3f27763588fb627f5b6d5e523c18adec44
eric <eric@xcom.(none)>
2015-01-04 10:57:48 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+#HelloJar
+
+Source code of the Android Library Project Application.
+
+For build instruction, please read http://bookofzeus.com/articles/android/import-android-library-project-android-studio
+
---
+++ src/main/java/appdemo/hellojar/MainActivity.java
@@ -0,0 +1,52 @@
+package appdemo.hellojar;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.mylibrary.LibraryDemo;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Create the element from the ID
+        TextView mHello = (TextView)findViewById(R.id.txt_hello);
+
+        // Load the Library object
+        LibraryDemo lib = new LibraryDemo();
+        CharSequence name = (CharSequence)lib.getSiteName();
+
+        // Set the text
+        mHello.setText(String.format(getString(R.string.hello_world), name));
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
